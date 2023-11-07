54f684a0c19f5cfe0cb8e155a7553262873afcb3
regis <regis.chevillot@inari.io>
2023-11-07 10:50:28 +0100

    vite config

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ import {
 import validators from "./utils/validators.js";
 import fieldComponents from "./utils/fieldsLoader";
 import abstractField from "./fields/abstractField";
+import fieldInput from "./fields/core/fieldInput.vue";
+import fieldCheckbox from "./fields/core/fieldCheckbox.vue";
+import fieldChecklist from "./fields/core/fieldChecklist.vue";
+import fieldLabel from "./fields/core/fieldLabel.vue";
+import fieldRadios from "./fields/core/fieldRadios.vue";
+import fieldSelect from "./fields/core/fieldSelect.vue";
+import fieldSubmit from "./fields/core/fieldSubmit.vue";
+import fieldTextArea from "./fields/core/fieldTextArea.vue";
+import fieldUpload from "./fields/core/fieldUpload.vue";
 
 const install = (Vue, options) => {
 	Vue.component("VueFormGenerator", component);
@@ -29,6 +38,15 @@ export {
 	validators,
 	abstractField,
 	fieldComponents,
+	fieldInput,
+	fieldCheckbox,
+	fieldChecklist,
+	fieldLabel,
+	fieldRadios,
+	fieldSelect,
+	fieldSubmit,
+	fieldTextArea,
+	fieldUpload,
 	install
 }
 
